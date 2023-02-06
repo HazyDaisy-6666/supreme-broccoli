708a09e3a7d5defe55517442c7eb498e6f48024a
Dale Baromen <dale30@discovery.co.za>
2023-02-06 14:47:03 +0200

    progress

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function testFunction() {
-window.alert ("js is working");
-}
+function toggleNav() {
+    const outerGrid = document.querySelector('.outer-grid');
+    outerGrid.classList.toggle('outer-grid-expand');
+    
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('hide-nav');
+    }
+
+    function toggleForm() {
+        const loginForm = document.querySelector('.form-popup');
+        loginForm.classList.toggle('.form-reveal');
+      }
+      
+      
