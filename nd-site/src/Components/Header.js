6b67a14c8f65e0a7a3b1905fbd9bab3cbf640296
Dale Baromen <dale30@discovery.co.za>
2023-02-17 07:41:27 +0200

    MOVED OUT OF ONEDRIVE

---
+++ nd-site/src/Components/Header.js
@@ -0,0 +1,37 @@
+<header id="top-nav">
+	<div id="top-nav-left">
+		<i
+			id="nav-button"
+			className="fa-solid fa-bars icon"
+			onclick="toggleNav()"
+		></i>
+		<i
+			className="fa-solid fa-house icon"
+			onclick="home()"
+		></i>
+	</div>
+	<div id="top-nav-center">
+		<h2>Neurodivergents Unite</h2>
+	</div>
+	<div id="top-nav-right">
+		<p
+			className="icon"
+			href="blank"
+		>
+			About{" "}
+		</p>
+		<p
+			className="icon"
+			href="blank"
+		>
+			Contact{" "}
+		</p>
+		<p
+			className="icon"
+			href="blank"
+			onclick="toggleForm()"
+		>
+			Log in
+		</p>
+	</div>
+</header>;
---
+++ nd-site/src/Components/SideNav.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function SideNav() {
+	return (
+		<nav
+			className="hide-content"
+			id="left_nav"
+		>
+			<p>
+				<i className="fa-sharp fa-solid fa-brain icon"></i> Austism
+			</p>
+			<p>
+				<i className="fa-sharp fa-solid fa-brain icon"></i> ADHD
+			</p>
+			<p>
+				<i className="fa-sharp fa-solid fa-brain icon"></i> Anxiety
+			</p>
+		</nav>
+	);
+}
